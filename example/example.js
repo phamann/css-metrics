44d7b234b1593af8eac092fb01ad9fd96488697d
Robert Quinlivan <rquinlivan@groupon.com>
2013-03-12 11:14:17 -0500

    More examples

--- example/example.js
+++ example/example.js
@@ -1,7 +1,29 @@
 var CSSMetrics = require('../css-metrics');
 
-var myMetrics = new CSSMetrics('./css/style.css');
+var stats = new CSSMetrics('./css/style.css').stats();
 
-if (myMetrics.stats().rules > 3 ) {
-  console.log("oh noes, we have more than three rules! time to do some cleanup");
+var fail = "[FAIL]",
+    failure = false;
+
+var maxStats = {
+  rules: 5,
+  totalSelectors: 5,
+  averageSelectors: 2
+};
+
+
+console.log("Checking CSS metrics...");
+
+var str;
+
+for (var k in maxStats) {
+  str = "";
+  if(stats[k] > maxStats[k]) {
+     str += fail;
+     failure = true;
+  }
+  str += " " + k + " = " + stats[k]
+  console.log(str);
 }
+
+process.exit(failure);
